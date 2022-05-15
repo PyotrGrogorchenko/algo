b75c25c3a09c95d51a960d4cf2ae55c5ca1960a8
Pyotr Grigorchenko <PePeGrigorchenko@yandex.ru>
2022-05-16 02:17:41 +0300

    15052022

---
+++ src/basis/recursion/backtracking.js
@@ -0,0 +1,22 @@
+const queens = (n, row, col, diag1, diag2) => {
+  if (!row) row = 0
+  if (!col) col = new Array(n).fill(0)
+  if (!diag1) diag1 = new Array(n * 2 - 1).fill(0)
+  if (!diag2) diag2 = new Array(n * 2 - 1).fill(0)
+
+  if (row === n) return true
+
+  let res = 0
+  for (let column = 0; column < n; column++) {
+    if (!col[column] && !diag1[column + row] && !diag2[row - column + n - 1]) {
+      col[column] = diag1[column + row] = diag2[row - column + n - 1] = 1
+      res += queens(n, row + 1, col, diag1, diag2)
+      col[column] = diag1[column + row] = diag2[row - column + n - 1] = 0
+    }
+  }
+  return res
+}
+
+for (let n = 1; n <= 12; n++) {
+  console.log(n + '=', queens(n))
+}
