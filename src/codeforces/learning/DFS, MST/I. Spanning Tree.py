5a947741d1db91de6e738b3c396af880abbd064a
Pyotr Grigorchenko <grigorchenko.p@compmechlab.ru>
2023-09-26 09:41:28 -0400

    26092023

--- src/codeforces/learning/DFS, MST/I. Spanning Tree.py
+++ src/codeforces/learning/DFS, MST/I. Spanning Tree.py
@@ -2,21 +2,57 @@ import os
 import sys
 
 
-def solve(n, vertexes):
-    print('solve')
+def dsu_find(p, v):
+    if p[v] == v:
+        return v
+    return dsu_find(p, p[v])
+
+
+def dsu_unite(p, a, b):
+    a = dsu_find(p, a)
+    b = dsu_find(p, b)
+    if a != b:
+        p[a] = p[b]
+
+
+def solve(n, points):
+    g = []
+    for i in range(n):
+        x1, y1 = points[i]
+        for j in range(n):
+            x2, y2 = points[j]
+            x = abs(x1 - x2)
+            y = abs(y1 - y2)
+            d = (x**2 + y**2) ** 0.5
+            if d == 0:
+                continue
+            g.append((i, j, d))
+    g.sort(key=lambda v: v[2])
+
+    res = 0
+    p = [i for i in range(n)]
+    for e in g:
+        a = e[0]
+        b = e[1]
+        d = e[2]
+        if dsu_find(p, a) != dsu_find(p, b):
+            res += d
+            dsu_unite(p, a, b)
+
+    print(str(round(res, 10)))
 
 
 def read_data(data):
     n = int(data.readline().strip())
-    vertexes = []
+    points = []
     for i in range(n):
-        vertexes.append(list(map(int, data.readline().strip().split())))
-    solve(n, vertexes)
+        points.append(list(map(int, data.readline().strip().split())))
+    solve(n, points)
 
 
-for file_name in os.listdir(path='tests'):
-    file_data = open(f'tests/{file_name}')
-    read_data(file_data)
+# for file_name in os.listdir(path='tests'):
+#     file_data = open(f'tests/{file_name}')
+#     read_data(file_data)
 
-# read_data(sys.stdin)
+read_data(sys.stdin)
 
